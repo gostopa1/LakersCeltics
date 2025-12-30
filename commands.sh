1916df17270aea245a6ae24596f86e7a317b1eb5
Athanasios Gotsopoulos <tan@neuraldsp.com>
2025-12-30 18:03:47 +0200

    Current work in progress for commands.sh

--- commands.sh
+++ commands.sh
@@ -1,15 +1,14 @@
 # Download the ZIP file of the game
 wget https://archive.org/download/msdos_Lakers_versus_Celtics_and_the_NBA_Playoffs_1989/Lakers_versus_Celtics_and_the_NBA_Playoffs_1989.zip
 # Unzip
-unzip Lakers_versus_Celtics_and_the_NBA_Playoffs_1989.zip
+# unzip Lakers_versus_Celtics_and_the_NBA_Playoffs_1989.zip
 # Unzip and overwrite 
 unzip -o Lakers_versus_Celtics_and_the_NBA_Playoffs_1989.zip 
-
 
 # Extract all images from teams
 python scripts/extract_all_team_images.py
 
-# SUBDAT
+# Extract from subdat SUBDAT
 python ./scripts/visualize_4bit.py Lakersve/SUBDAT --offset 0  --length 60 --rows 7 --cols 96 --image ./images/players-stats.png
 python ./scripts/visualize_4bit.py Lakersve/SUBDAT --offset 336  --length 60 --rows 7 --cols 96 --image ./images/year_stats-offense.png
 python ./scripts/visualize_4bit.py Lakersve/SUBDAT --offset 672  --length 60 --rows 7 --cols 96 --image ./images/year_stats-defense.png
@@ -18,10 +17,12 @@ python ./scripts/visualize_4bit.py Lakersve/SUBDAT --offset 672  --length 60 --r
 
 python ./scripts/visualize_4bit.py Lakersve/TP5 --offset 5212  --length 60 --rows 5 --cols 24 --image ./block.png --overlay
 #python ./scripts/set_values.py Lakersve/TP5 --offset 5212  --length 6 --values "1"
-unzip -o Lakers_versus_Celtics_and_the_NBA_Playoffs_1989.zip 
+
+# Setting values
 python ./scripts/set_values.py Lakersve/TP1 --offset 5224  --length 72 --values "01"
 python ./scripts/set_values.py Lakersve/TP5 --offset 5260  --length 24 --values "FF"
 python ./scripts/set_values.py Lakersve/TP1 --offset 5260  --length 24 --values "00"
+
 python ./scripts/visualize_4bit.py Lakersve/TP5 --offset 5224  --length 12 --rows 4 --cols 24 --image ./block.png --overlay
 
 open /Users/tan/DosStuff/dosbox-x-macosx-arm64-20251007154914/dosbox-x/dosbox-x.app --args /Users/tan/DosStuff/LakersCeltics/Lakersve/BBALL.EXE
@@ -30,6 +31,25 @@ open /Users/tan/DosStuff/dosbox-x-macosx-arm64-20251007154914/dosbox-x/dosbox-x.
 # git remote set-url origin git@github-personal:gostopa1/LakersCeltics.git
 
 # Test scenario: Add last row of attributes all FF for Lakers and all 00 for Celtics. Let em play
+unzip -o Lakers_versus_Celtics_and_the_NBA_Playoffs_1989.zip 
+
+python ./scripts/set_values.py Lakersve/TP5 --offset 5224  --length 24 --values "18"
+python ./scripts/set_values.py Lakersve/TP1 --offset 5224  --length 24 --values "02"
+
+
+python ./scripts/set_values.py Lakersve/TP5 --offset 5236  --length 24 --values "12"
+python ./scripts/set_values.py Lakersve/TP1 --offset 5236  --length 24 --values "00"
+
+#3rd stat category
+python ./scripts/set_values.py Lakersve/TP5 --offset 5248  --length 24 --values "28"
+python ./scripts/set_values.py Lakersve/TP1 --offset 5248  --length 24 --values "08"
+
+# Free throws?
 python ./scripts/set_values.py Lakersve/TP5 --offset 5260  --length 24 --values "FF"
 python ./scripts/set_values.py Lakersve/TP1 --offset 5260  --length 24 --values "00"
 python ./scripts/visualize_4bit.py Lakersve/TP5 --offset 5224  --length 12 --rows 4 --cols 24 --image ./block.png --overlay
+python ./scripts/visualize_4bit.py Lakersve/TP5 --offset 5224  --length 12 --rows 4 --cols 24 --image ./tests/Lakers_5224-5272.png --overlay
+python ./scripts/visualize_4bit.py Lakersve/TP1 --offset 5224  --lengbth 12 --rows 4 --cols 24 --image ./tests/Celtics_5224-5272.png --overlay
+python ./scripts/visualize_4bit.py Lakersve/TP1 --offset 3988  --length 12 --rows 4 --cols 24 --image ./tests/block.png --overlay
+
+dosbox.exe -noconsole -c "mount T 'D:\My Folder1\My Folder2'" -c "T:" -c "myapp.exe -parameter1 file1.ex1 file2.ex2" -c "exit"
